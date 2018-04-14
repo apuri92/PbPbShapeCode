64d9df35f40467780699c0e9b79db08f9d7a2eea
Akshat Puri <akshat.puri@cern.ch>
2018-04-14 01:02:20 -0500

    fixed the UE used (using UE_HP for both data and MC)

--- output_dev/unfold/auto.sh
+++ output_dev/unfold/auto.sh
@@ -21,10 +21,10 @@ function auto {
 
 mode=$1 #if running full unfolding as well
 
-# auto PbPb 1
+auto PbPb 1
 auto pp 1
 root -b -q "comp_ChPS.c(1)"
 
-# auto PbPb 0
+auto PbPb 0
 auto pp 0
 root -b -q "comp_ChPS.c(0)"
